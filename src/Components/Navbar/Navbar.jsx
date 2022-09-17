d8979d179d7d7c3fdcba059881f31f7f1f042612
tomas oliver <101881995+tomi204@users.noreply.github.com>
2022-09-16 23:07:56 -0300

    navbar

---
+++ src/Components/Navbar/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import "./Navbar.css"
+const Navbar = () => {
+    return (
+        <div className='navbar'>
+            <FontAwesomeIcon icon="fa-sharp fa-solid fa-moon-stars" />
+        </div>
+    )
+}
+
+export default Navbar
