f2582959c175ef82cb7401a666a55d96771281d6
tomas oliver <101881995+tomi204@users.noreply.github.com>
2022-09-18 22:34:45 -0300

    navbar agregada, no terminada

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,13 +1,28 @@
 import React from 'react'
-import { FaMoon } from "react-icons/fa";
-
+import { FaIdCard, FaMoon, FaSun } from "react-icons/fa";
 import "./Navbar.css"
 const Navbar = () => {
     return (
         <div className='navbar'>
-
-            <FaMoon className='moon' />
-
+            <li className='navbar-list'>
+                <a href="#navbar">
+                    <h3>Home</h3>
+                </a>
+                <a href="">
+                    <h3>Projects</h3>
+                </a>
+                <a href="">
+                    <h3>Skills</h3>
+                </a>
+                <a href="">
+                    <FaMoon className='moon' />
+                </a>
+                <a href="">
+                    <FaSun />
+                </a>
+                <a href="">
+                    <FaIdCard /></a>
+            </li>
         </div>
     )
 }
--- src/Home/Home.jsx
+++ src/Home/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import Navbar from '../Components/Navbar/Navbar'
 export const Home = () => {
     return (
         <section className='home'>
-
+            <Navbar />
         </section>
     )
 }
